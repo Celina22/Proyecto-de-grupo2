b917e8f4d02366ba63b7b4e62b1eec5a409a5101
gmfrancisco99 <gfranciscom99@gmail.com>
2020-08-05 10:04:13 -0300

    Proyecto con Login.

---
+++ MiPrimerProyecto/MiPrimerProyecto/frm_login.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace MiPrimerProyecto
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.lblClave = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(60, 115);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(150, 115);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.lblUsuario.Location = new System.Drawing.Point(57, 47);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(46, 13);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(109, 44);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(116, 20);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(109, 70);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.PasswordChar = '*';
+            this.txtClave.Size = new System.Drawing.Size(116, 20);
+            this.txtClave.TabIndex = 3;
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.lblClave.Location = new System.Drawing.Point(66, 73);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(37, 13);
+            this.lblClave.TabIndex = 0;
+            this.lblClave.Text = "Clave:";
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.lblClave);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnIngresar);
+            this.MaximumSize = new System.Drawing.Size(500, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 200);
+            this.Name = "frm_login";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.Label lblClave;
+    }
+}
+
---
+++ MiPrimerProyecto/MiPrimerProyecto/frm_login.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerProyecto
+{
+    public partial class frm_login : Form
+    {
+        string user = "admin";
+        string pass = "123";
+        public frm_login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            //if (this.txtUsuario.Text == "")
+            if (this.txtUsuario.Text == string.Empty)
+            {
+                MessageBox.Show("Debe ingresar un usuario.");
+                this.txtUsuario.Focus();
+                return;
+            }
+            //if (this.txtClave.Text == "")
+            if (string.IsNullOrEmpty(this.txtClave.Text))
+            {
+                MessageBox.Show("Debe ingresar una clave.");
+                this.txtClave.Focus();
+                return;
+            }
+
+            //Validar usuario y clave.
+            string msj = "";
+            if (this.txtUsuario.Text == this.user && this.txtClave.Text == this.pass)
+            {
+                msj = "Login OK. Bienvenido, " + user + ".";
+                MessageBox.Show(msj, "Ingreso al sistema", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                msj = "Usuario y/o clave incorrectos.";
+                MessageBox.Show(msj, "Ingreso al sistema", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                this.txtUsuario.Text = string.Empty;
+                this.txtClave.Text = string.Empty;
+                this.txtUsuario.Focus();
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
