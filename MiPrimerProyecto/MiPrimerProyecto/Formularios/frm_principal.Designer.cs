1bc57bcd3444bedaab3b078dc5c1ca0c31e89f2d
maube1905 <maubecinagustin@gmail.com>
2020-08-09 20:03:58 -0300

    Se hacen cambios esteticos en la pantalla principal

--- MiPrimerProyecto/MiPrimerProyecto/Formularios/frm_principal.Designer.cs
+++ MiPrimerProyecto/MiPrimerProyecto/Formularios/frm_principal.Designer.cs
@@ -28,7 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblBienvenidos = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(588, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // lblBienvenidos
+            // 
+            this.lblBienvenidos.AutoSize = true;
+            this.lblBienvenidos.ForeColor = System.Drawing.Color.LightCoral;
+            this.lblBienvenidos.Location = new System.Drawing.Point(198, 19);
+            this.lblBienvenidos.Name = "lblBienvenidos";
+            this.lblBienvenidos.Size = new System.Drawing.Size(215, 13);
+            this.lblBienvenidos.TabIndex = 1;
+            this.lblBienvenidos.Text = "BIENVENIDOS A MI PRIMER PROGRAMA";
             // 
             // frm_principal
             // 
@@ -36,14 +55,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBienvenidos);
+            this.Controls.Add(this.dateTimePicker1);
             this.Name = "frm_principal";
             this.Text = "Menú Principal";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frm_principal_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblBienvenidos;
     }
 }
