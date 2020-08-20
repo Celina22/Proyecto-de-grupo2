07a709720fc1b7e080330800d302d6ce80c5aeb7
gmfrancisco99 <gfranciscom99@gmail.com>
2020-08-20 11:21:31 -0300

    Sacado el constructor de Bug.

--- MiPrimerProyecto/MiPrimerProyecto/Clases/Bug.cs
+++ MiPrimerProyecto/MiPrimerProyecto/Clases/Bug.cs
@@ -20,21 +20,6 @@ namespace MiPrimerProyecto.Clases
         private int id_estado;
         private bool borrado;
 
-        public Bug(int id_bug, string titulo, string descripcion, DateTime fecha_alta, int id_usuario_responsable, int id_usuario_asignado,
-                   int id_producto, int id_prioridad, int id_criticidad, int id_estado, bool borrado)
-        {
-            this.Id_bug = id_bug;
-            this.Titulo = titulo;
-            this.Descripcion = descripcion;
-            this.Fecha_alta = fecha_alta;
-            this.Id_usuario_responsable = id_usuario_responsable;
-            this.Id_usuario_asignado = id_usuario_asignado;
-            this.Id_producto = id_producto;
-            this.Id_prioridad = id_prioridad;
-            this.Id_criticidad = id_criticidad;
-            this.Id_estado = id_estado;
-            this.Borrado = borrado;
-        }
 
         public int Id_bug { get => id_bug; set => id_bug = value; }
         public string Titulo { get => titulo; set => titulo = value; }
