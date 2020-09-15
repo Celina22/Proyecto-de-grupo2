95ae0e8231e0243fba095e517a1308ca733dc13c
JoaquinCostamagna <jncostamagna@gmail.com>
2020-09-15 15:22:57 -0300

    Cambio de colores de iconos

--- LPCFacturas/ProyectoBugs/Properties/Resources.Designer.cs
+++ LPCFacturas/ProyectoBugs/Properties/Resources.Designer.cs
@@ -139,5 +139,15 @@ namespace ProyectoBugs.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Salir {
+            get {
+                object obj = ResourceManager.GetObject("Salir", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
