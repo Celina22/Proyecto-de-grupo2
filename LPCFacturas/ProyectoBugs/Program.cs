f6ca9057559fb48e9d5658768e0f600c0b4a2322
Gastón Muñoz <gfranciscom99@gmail.com>
2020-10-14 10:35:45 -0300

    Cambio de formulario de reporte de proyectos

--- LPCFacturas/ProyectoBugs/Program.cs
+++ LPCFacturas/ProyectoBugs/Program.cs
@@ -18,7 +18,7 @@ namespace LPCFacturas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmPrincipal());
+            Application.Run(new frmReporteListadoProyectos());
         }
     }
 }
